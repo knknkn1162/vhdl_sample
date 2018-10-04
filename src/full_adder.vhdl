1f44bc2e33e19ce14ad40ac340f0643562ca5ef2
knknkn1162 <knknkn1162@gmail.com>
2018-10-04 17:25:06 +0900

    full_adder.vhdl

--- src/full_adder.vhdl
+++ src/full_adder.vhdl
@@ -1,17 +1,19 @@
-library ieee;
-use ieee.std_logic_1164.all;
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
 
 entity full_adder is
-    port (
-        a  : in  std_logic;
-        b  : in  std_logic;
-        ci : in  std_logic;
-        s  : out std_logic;
-        co : out std_logic);
+  port (
+    a : in std_logic;
+    b : in std_logic;
+    ci : in std_logic;
+    s : out std_logic;
+    co : out std_logic;
+       );
 end;
 
 architecture behavioral of full_adder is
 begin
-    s <= a xor b xor ci;
-    co <= (a and b) or ((a xor b) and ci);
+  s <= a xor b xor ci;
+  co <= (a and b) or ((a xor b) and ci);
 end;
+
