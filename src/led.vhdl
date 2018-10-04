cb390a50d10b13b706895d4c3517d4f2dfe6dc87
knknkn <knknkn1162@gmail.com>
2018-10-04 11:09:51 +0900

    try led & led_tb

--- src/led.vhdl
+++ src/led.vhdl
@@ -1,16 +1,17 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
-
 entity led is
-  Port ( switch_0 : in STD_LOGIC;
-         switch_1 : in STD_LOGIC;
-         LED_0 : out STD_LOGIC;
-         LED_1 : out STD_LOGIC);
+  Port (switch1 : in STD_LOGIC;
+        switch2 : in STD_LOGIC;
+        led1 : out STD_LOGIC;
+        -- must not have semicolon
+        led2 : out STD_LOGIC
+  );
 end led;
 
 architecture Behavioral of led is
 begin
-  LED_0 <= switch_0;
-  LED_1 <= switch_1;
+  led1 <= switch1;
+  led2 <= switch2;
 end Behavioral;
---
+++ src/led_tb.vhdl
@@ -0,0 +1,51 @@
+library IEEE;
+use IEEE.STd_LOGIC_1164.ALL;
+
+entity led_tb is
+end led_tb;
+
+architecture behavior of led_tb is
+  component led is
+    Port (switch0 : in STD_LOGIC;
+          switch1 : in STD_LOGIC;
+          led0 : out STD_LOGIC;
+          -- must not have semicolon
+          led1 : out STD_LOGIC
+    );
+  end component;
+
+  signal switch0 : STD_LOGIC := '0';
+  signal switch1 : STD_LOGIC := '0';
+
+  signal led0 : STD_LOGIC;
+  signal led1 : STD_LOGIC;
+
+begin
+  uut: led port map (
+
+    switch0 => switch0,
+    switch1 => switch1,
+    led0 => led0,
+    led1 => led1
+  );
+
+  stim_proc: process
+  begin
+    wait for 100 ns;
+    -- initialize input
+    switch0 <= '0';
+    switch1 <= '0';
+    wait for 10 ns;
+    switch0 <= '1';
+    switch1 <= '0';
+    wait for 10 ns;
+    switch0 <= '0';
+    switch1 <= '1';
+    wait for 10 ns;
+    switch0 <= '1';
+    switch1 <= '1';
+    wait for 10 ns;
+
+    wait;
+  end process;
+end;
