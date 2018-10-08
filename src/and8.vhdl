da1da12e5883df100d9aba49d22ec33a95481980
knknkn <knknkn1162@gmail.com>
2018-10-08 21:18:35 +0900

    modify and8

--- src/and8.vhdl
+++ src/and8.vhdl
@@ -7,8 +7,19 @@ entity and8 is
     y: out std_logic
   );
 end entity;
-
 architecture synth of and8 is
+
+  -- https://stackoverflow.com/questions/20296276/and-all-elements-of-an-n-bit-array-in-vhdl
+  function and_reduct(slv : in std_logic_vector) return std_logic is
+    variable res_v : std_logic := '1';  -- Null slv vector will also return '1'
+  begin
+    for i in slv'range loop
+      res_v := res_v and slv(i);
+    end loop;
+    return res_v;
+  end function;
+
 begin
-  y <= a(7) and a(6) and a(5) and a(4) and a(3) and a(2) and a(1) and a(0);
+  y <= and_reduct(a);
+  --y <= a(7) and a(6) and a(5) and a(4) and a(3) and a(2) and a(1) and a(0);
 end architecture;
