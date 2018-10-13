8f24c2bfe8329a1f8eef7cdb879ac6ef7cacf9fa
knknkn <knknkn1162@gmail.com>
2018-10-13 18:13:54 +0900

    fix imips with lw & sw

--- src/imips/imips.vhdl
+++ src/imips/imips.vhdl
@@ -9,8 +9,9 @@ entity imips is
     -- for testbench
     pc : out std_logic_vector(31 downto 0);
     instr : out std_logic_vector(31 downto 0);
-    rt : out std_logic_vector(31 downto 0);
-    aluout : out std_logic_vector(31 downto 0)
+    rs, rt : out std_logic_vector(31 downto 0);
+    aluout : out std_logic_vector(31 downto 0);
+    data : out std_logic_vector(31 downto 0)
        );
 end entity;
 
@@ -37,9 +38,8 @@ architecture behavior of imips is
       a1 : in std_logic_vector(4 downto 0);
       rd1 : out std_logic_vector(31 downto 0);
       -- 20:16(read)
-      -- a2 : in std_logic_vector(4 downto 0);
-      -- rd2 : out std_logic_vector(31 downto 0);
-
+      a2 : in std_logic_vector(4 downto 0);
+      rd2 : out std_logic_vector(31 downto 0);
       -- 20:16(write)
       a3 : in std_logic_vector(4 downto 0);
       wd3 : in std_logic_vector(31 downto 0);
@@ -62,10 +62,25 @@ architecture behavior of imips is
       zero : out std_logic
         );
   end component;
-  signal instr0, rs, rt0, immext : std_logic_vector(31 downto 0);
+
+  component dmem
+    port (
+      clk : in std_logic;
+      -- write enable
+      we : in std_logic;
+      -- write data
+      wd : in std_logic_vector(31 downto 0);
+      addr: in std_logic_vector(31 downto 0);
+      -- read data
+      rd : out std_logic_vector(31 downto 0)
+    );
+  end component;
+  signal instr0, rs0, rt0, immext, aluout0, data0 : std_logic_vector(31 downto 0);
   signal res : std_logic_vector(31 downto 0);
   signal pc0 : std_logic_vector(31 downto 0); -- buffer
   signal pcnext : std_logic_vector(31 downto 0);
+  signal regfile_we : std_logic;
+  signal dmem_we : std_logic;
 
 begin
   -- TODO: impl program counter
@@ -80,14 +95,34 @@ begin
   );
   instr <= instr0;
 
+  process(instr0) is
+  begin
+    case (instr0(31 downto 26)) is
+      -- lw instruction(0x23)
+      when "100011" =>
+        regfile_we <= '1';
+        dmem_we <= '0';
+      -- sw instruction(0x2B)
+      when "101011" =>
+        regfile_we <= '0';
+        dmem_we <= '1';
+      when others =>
+        regfile_we <= '0';
+        dmem_we <= '0';
+    end case;
+  end process;
+
   reg0 : regfile port map (
     clk => clk,
     a1 => instr0(25 downto 21),
-    rd1 => rt0, -- out
+    rd1 => rs0, -- out
+    a2 => instr0(20 downto 16),
+    rd2 => rt0, -- out
     a3 => instr0(20 downto 16),
-    wd3 => res,
-    we3 => '1'
+    wd3 => data0,
+    we3 => regfile_we
   );
+  rs <= rs0;
   rt <= rt0;
 
   sgnext0 : sgnext port map (
@@ -96,11 +131,20 @@ begin
   );
 
   alu0: alu port map (
-    a => rt0,
+    a => rs0,
     b => immext,
     f => "010",
-    y => res -- zero port is ignored
+    y => aluout0
   );
-  aluout <= res;
+  aluout <= aluout0;
+
+  dmem0: dmem port map (
+    clk => clk,
+    we => dmem_we,
+    wd => rt0,
+    addr => aluout0,
+    rd => data0
+  );
+  data <= data0;
 
 end architecture;
--- src/imips/imips_tb.vhdl
+++ src/imips/imips_tb.vhdl
@@ -10,24 +10,33 @@ architecture behavior of imips_tb is
     port (
       clk, reset : in std_logic;
       addr : in std_logic_vector(31 downto 0);
+      -- for testbench
       pc : out std_logic_vector(31 downto 0);
       instr : out std_logic_vector(31 downto 0);
-      rt : out std_logic_vector(31 downto 0);
-      aluout : out std_logic_vector(31 downto 0)
-        );
+      rs, rt : out std_logic_vector(31 downto 0);
+      aluout : out std_logic_vector(31 downto 0);
+      data : out std_logic_vector(31 downto 0)
+    );
   end component;
   signal clk, reset : std_logic;
   signal addr : std_logic_vector(31 downto 0);
   signal pc : std_logic_vector(31 downto 0);
   signal instr : std_logic_vector(31 downto 0);
-  signal rt : std_logic_vector(31 downto 0);
+  signal rs, rt : std_logic_vector(31 downto 0);
   signal aluout : std_logic_vector(31 downto 0);
+  signal data : std_logic_vector(31 downto 0);
   signal clk_period : time := 10 ns;
   signal stop : boolean;
 
 begin
   uut: imips port map (
-    clk, reset, addr, pc, instr, rt, aluout
+    clk, reset,
+    addr,
+    pc,
+    instr,
+    rs, rt,
+    aluout,
+    data
   );
 
   clk_process: process
@@ -42,19 +51,25 @@ begin
   stim_proc: process
   begin
     -- wait until rising_edge
+    addr <= X"00000000";
     wait for clk_period/2;
-    reset <= '1'; wait for 1 ns;
-    addr <= X"00000000"; reset <= '0';
+    reset <= '1'; wait for 1 ns;reset <= '0';
+
+    -- lw
     wait for clk_period/2; 
     assert pc = X"00000000";
-    assert instr = X"20100005";
-    assert rt = X"00000000";
-    assert aluout = X"00000005";
+    assert instr = X"8c100040";
+    assert rs = X"00000000";
+    assert aluout = X"00000040";
+    assert data = X"000000FF";
+
+    -- sw
     wait for clk_period;
     assert pc = X"00000004";
-    assert instr = X"2211000a";
-    assert rt = X"00000005";
-    assert aluout = X"0000000f";
+    assert instr = X"ac10003c";
+    assert rs = X"00000000";
+    assert rt = X"000000ff";
+    assert aluout = X"0000003c";
 
     -- success message
     assert false report "end of test" severity note;
