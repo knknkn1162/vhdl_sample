af5d87c37a53f290976cbc585364bda320545b26
knknkn <knknkn1162@gmail.com>
2018-10-14 21:59:28 +0900

    generalize sltn_tb

--- src/imips/sltn_tb.vhdl
+++ src/imips/sltn_tb.vhdl
@@ -14,7 +14,7 @@ architecture behavior of sltn_tb is
         );
   end component;
 
-  signal a : std_logic_vector(31 downto 0) := X"00000001";
+  signal a : std_logic_vector(31 downto 0) := X"00000002";
   signal n : std_logic_vector(4 downto 0);
   signal y : std_logic_vector(31 downto 0);
 begin
@@ -25,10 +25,11 @@ begin
   stim_proc: process
   begin
     wait for 20 ns;
-    n <= "00000"; wait for 10 ns; assert y <= X"00000004";
-    n <= "00010"; wait for 10 ns; assert y <= X"00000004";
-    n <= "00100"; wait for 10 ns; assert y <= X"00000010";
-    n <= "11111"; wait for 10 ns; assert y <= X"80000000";
+    n <= "00000"; wait for 10 ns; assert y <= X"00000002";
+    n <= "00010"; wait for 10 ns; assert y <= X"00000008";
+    n <= "00100"; wait for 10 ns; assert y <= X"00000020";
+    n <= "11110"; wait for 10 ns; assert y <= X"80000000";
+    n <= "11111"; wait for 10 ns; assert y <= X"00000000";
     -- success message
     assert false report "end of test" severity note;
     wait;
