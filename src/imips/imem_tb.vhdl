5ad0f113e345c583ae5d79f97ecb4c9c1f7bdc9a
knknkn <knknkn1162@gmail.com>
2018-10-15 21:50:47 +0900

    store instruction memory

--- src/imips/imem_tb.vhdl
+++ src/imips/imem_tb.vhdl
@@ -1,5 +1,6 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
 
 entity imem_tb is
 end entity;
@@ -13,6 +14,8 @@ architecture behavior of imem_tb is
   end component;
   signal idx : std_logic_vector(5 downto 0);
   signal rd : std_logic_vector(31 downto 0);
+  constant N : integer := 17;
+  constant DIM : integer := 6;
 begin
   uut : imem port map (
     idx, rd
@@ -21,9 +24,10 @@ begin
   stim_proc : process
   begin
     wait for 20 ns;
-    idx <= "000000"; wait for 10 ns; assert rd = X"20100005";
-    idx <= "000001"; wait for 10 ns; assert rd = X"2211000a";
-    idx <= "000010"; wait for 10 ns; assert rd = X"00000000";
+    for i in 0 to N loop
+      idx <= std_logic_vector(to_unsigned(i, DIM)); wait for 10 ns; assert rd /= X"00000000";
+    end loop;
+    idx <= std_logic_vector(to_unsigned(N+1, DIM)); wait for 10 ns; assert rd = X"00000000";
     -- success message
     assert false report "end of test" severity note;
     wait;
