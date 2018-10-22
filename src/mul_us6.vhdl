f05dbc4dd69d9bce98b7fa8cf4172f9edd9379ab
knknkn1162 <knknkn1162@gmail.com>
2018-10-22 16:22:02 +0900

    unsigned*signed

---
+++ src/mul_us6.vhdl
@@ -0,0 +1,17 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
+
+entity mul6 is
+  generic(N: integer);
+  port (
+    a : in std_logic_vector(N-1 downto 0);
+    b : in std_logic_vector(N-1 downto 0);
+    c : out std_logic_vector(2*N-1 downto 0)
+  );
+end entity;
+
+architecture behavior of mul6 is
+begin
+  c <= std_logic_vector(to_signed(to_integer(unsigned(a)) * to_integer(signed(b)), c'length));
+end architecture;
