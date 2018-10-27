7aab42f9eaca30aa4c2e74a0b5e1239920a3d4be
knknkn <knknkn1162@gmail.com>
2018-10-27 19:10:33 +0900

    bugfix softmax_with_loss

--- src/nn02/backward/softmax_with_loss.vhdl
+++ src/nn02/backward/softmax_with_loss.vhdl
@@ -14,18 +14,24 @@ end entity;
 
 architecture testbench of softmax_with_loss is
   constant zero_or_one : arr_type(0 to 1) := ((others => '0'), (others => '1'));
-  signal m : integer;
+  signal m : std_logic_vector(SIZE downto 0); --integer;
+
 begin
   process(y, t)
-    variable tmp : integer range -2**(SIZE) to 2**SIZE-1;
+    --variable tmp : integer range -2**(SIZE) to 2**SIZE-1;
+    variable tmp : std_logic_vector(SIZE downto 0);
   begin
   if is_X(t) or is_X(y(0)) then
     a <= (others => (others => '-'));
   else
     for i in 0 to N-1 loop
       -- see https://stackoverflow.com/questions/34039510/std-logic-to-integer-conversion#comment84596633_34039685
-      tmp := to_integer(signed(unsigned(y(i)) - unsigned(zero_or_one( to_integer(unsigned'('0' & t(i))) ))));
-      a(i) <= std_logic_vector(to_signed(tmp, SIZE+1));
+      -- unsigned("0" & y(i)) : can't resolve overload for operator "&"
+      tmp := std_logic_vector(unsigned('0' & y(i)) - unsigned('0' & zero_or_one( to_integer(unsigned'('0' & t(i))) )));
+      if i = 1 then
+        m <= tmp;
+      end if;
+      a(i) <= std_logic_vector(signed(tmp));
     end loop;
   end if;
   end process;
--- src/nn02/backward/softmax_with_loss_tb.vhdl
+++ src/nn02/backward/softmax_with_loss_tb.vhdl
@@ -20,6 +20,7 @@ architecture testbench of softmax_with_loss_tb is
   signal y : arr_type(0 to N-1);
   signal t : std_logic_vector(N-1 downto 0);
   signal a : arr1_type(0 to N-1);
+  signal a0 : std_logic_vector(SIZE downto 0);
 
 begin
   uut : softmax_with_loss generic map(N=>N)
@@ -32,6 +33,8 @@ begin
     wait for 20 ns;
     y <= (X"09", X"04", X"E6", X"04"); t <= "0100"; wait for 10 ns;
     assert a = ("0" & X"09", "0" & X"04", "1" & X"E7", "0" & X"04");
+    t <= "0010"; wait for 10 ns;
+    assert a = ("0" & X"09", "1" & X"05", "0" & X"E6", "0" & X"04");
     -- success message
     assert false report "end of test" severity note;
     wait;
