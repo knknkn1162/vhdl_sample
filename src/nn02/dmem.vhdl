d77643a832893073d82472ab67599d981e0f0104
knknkn <knknkn1162@gmail.com>
2018-10-28 16:05:21 +0900

    hide in image_x port

--- src/nn02/dmem.vhdl
+++ src/nn02/dmem.vhdl
@@ -1,55 +1,68 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
+
+package dmem_const_pkg is
+  constant IMAGE_SIZE : natural := 28;
+end package;
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
-use STD.textio.all;
+use work.dmem_const_pkg.ALL;
+use work.nn_pkg.ALL;
 
 entity dmem is
   port (
     a : in std_logic_vector(15 downto 0);
     id : out std_logic_vector(31 downto 0);
     cnt : out std_logic_vector(31 downto 0);
-    data : out std_logic_vector(7 downto 0)
+    -- image
+    --x : out arr_type(0 to IMAGE_SIZE*IMAGE_SIZE-1);
+    -- [0, 9)
+    t : out std_logic_vector(3 downto 0)
   );
 end entity;
 
 architecture behavior of dmem is
   constant N : natural := 60000;
   type char_file_type is file of character;
-  type ramtype is array(0 to N-1) of std_logic_vector(7 downto 0);
-  signal mem : ramtype;
-  signal count : std_logic_vector(31 downto 0);
+  type labeltype is array(0 to N-1) of std_logic_vector(3 downto 0);
+  -- type imgtype is array(0 to N-1) of std_logic_vector(SIZE*IMAGE_SIZE*IMAGE_SIZE - 1 downto 0);
+  signal label_mem : labeltype;
+  --signal data_mem : imgtype;
 
 begin
   process is
-      file file_in : char_file_type open read_mode is "./assets/train-labels-idx1-ubyte";
+      file label_file_in : char_file_type open read_mode is "./assets/train-labels-idx1-ubyte";
+      --file image_file_in : char_file_type open read_mode is "./assets/train-images-idx3-ubyte";
       variable char_buf : character;
       variable idx : natural;
   begin
     -- initialization
     for i in 3 downto 0 loop
-      read(file_in, char_buf);
+      read(label_file_in, char_buf);
       id(i*8+7 downto i*8) <= std_logic_vector(to_unsigned(character'pos(char_buf),8));
     end loop;
 
     for i in 3 downto 0 loop
-      read(file_in, char_buf);
-      count(i*8+7 downto i*8) <= std_logic_vector(to_unsigned(character'pos(char_buf),8));
+      read(label_file_in, char_buf);
+      cnt(i*8+7 downto i*8) <= std_logic_vector(to_unsigned(character'pos(char_buf),8));
     end loop;
 
-    cnt <= count;
-
-    while not endfile(file_in) loop
-      read(file_in, char_buf);
-      mem(idx) <= std_logic_vector(to_unsigned(character'pos(char_buf), 8));
-      idx := idx+1;
+    idx := 0;
+    while not endfile(label_file_in) loop
+      read(label_file_in, char_buf);
+      label_mem(idx) <= std_logic_vector(to_unsigned(character'pos(char_buf), 4));
+      idx := idx + 1;
     end loop;
-    file_close(file_in);
+    file_close(label_file_in);
 
     loop
       if is_X(a) then
-        data <= (others => '-');
+        --x <= (others => (others => '-'));
+        t <= (others => '-');
       else
-        data <= mem(to_integer(unsigned(a)));
+        t <= label_mem(to_integer(unsigned(a)));
       end if;
       wait on a;
     end loop;
--- src/nn02/dmem_tb.vhdl
+++ src/nn02/dmem_tb.vhdl
@@ -1,5 +1,7 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
+use work.nn_pkg.ALL;
+use work.dmem_const_pkg.ALL;
 
 entity dmem_tb is
 end entity;
@@ -11,29 +13,39 @@ architecture testbench of dmem_tb is
     a : in std_logic_vector(15 downto 0);
     id : out std_logic_vector(31 downto 0);
     cnt : out std_logic_vector(31 downto 0);
-    data : out std_logic_vector(7 downto 0)
+    -- image
+    --x : out arr_type(0 to IMAGE_SIZE*IMAGE_SIZE-1);
+    -- [0, 9)
+    t : out std_logic_vector(3 downto 0)
   );
   end component;
 
   signal a : std_logic_vector(15 downto 0);
   signal id, cnt : std_logic_vector(31 downto 0);
-  signal data : std_logic_vector(7 downto 0);
+  --signal x : arr_type(0 to IMAGE_SIZE*IMAGE_SIZE-1);
+  signal t : std_logic_vector(3 downto 0);
 
 begin
 
   uut : dmem port map (
     a => a,
-    id => id, cnt => cnt, data => data
+    id => id,
+    cnt => cnt,
+    --x => x,
+    t => t
   );
 
   stim_proc : process
   begin
     wait for 20 ns;
+    -- id= 2049, cnt = 60000
+    assert id = X"00000801";
+    assert cnt = X"0000ea60";
     -- assume to be assets/train-labels-idx1-ubyte
-    a <= X"0000"; wait for 10 ns; assert data = X"05";
-    a <= X"0001"; wait for 10 ns; assert data = X"00";
-    a <= X"0002"; wait for 10 ns; assert data = X"04";
-    a <= X"0003"; wait for 10 ns; assert data = X"01";
+    a <= X"0000"; wait for 10 ns; assert t = X"5";
+    a <= X"0001"; wait for 10 ns; assert t = X"0";
+    a <= X"0002"; wait for 10 ns; assert t = X"4";
+    a <= X"0003"; wait for 10 ns; assert t = X"1";
     -- success message
     assert false report "end of test" severity note;
     wait;
