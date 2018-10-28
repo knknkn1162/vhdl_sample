4587455e29c0bddfa97325abf6f8c6f11645eae0
knknkn <knknkn1162@gmail.com>
2018-10-28 14:25:37 +0900

    dmem with testbench

--- src/nn02/dmem.vhdl
+++ src/nn02/dmem.vhdl
@@ -5,9 +5,10 @@ use STD.textio.all;
 
 entity dmem is
   port (
-    a : in std_logic;
+    a : in std_logic_vector(15 downto 0);
     id : out std_logic_vector(31 downto 0);
-    cnt : out std_logic_vector(31 downto 0)
+    cnt : out std_logic_vector(31 downto 0);
+    data : out std_logic_vector(7 downto 0)
   );
 end entity;
 
@@ -16,14 +17,14 @@ architecture behavior of dmem is
   type char_file_type is file of character;
   type ramtype is array(0 to N-1) of std_logic_vector(7 downto 0);
   signal mem : ramtype;
-  signal load : std_logic;
+  signal count : std_logic_vector(31 downto 0);
 
 begin
-  load <= '1';
   process(a)
       file file_in : char_file_type open read_mode is "./assets/train-labels-idx1-ubyte";
       variable char_buf : character;
       variable idx : natural;
+      variable load : std_logic := '1';
   begin
     -- initialization
     if load = '1' then
@@ -34,18 +35,24 @@ begin
 
       for i in 3 downto 0 loop
         read(file_in, char_buf);
-        cnt(i*8+7 downto i*8) <= std_logic_vector(to_unsigned(character'pos(char_buf),8));
+        count(i*8+7 downto i*8) <= std_logic_vector(to_unsigned(character'pos(char_buf),8));
       end loop;
 
-      idx := 0;
+      cnt <= count;
 
-      for i in 0 to N-1 loop
+      while not endfile(file_in) loop
         read(file_in, char_buf);
-        mem(i) <= std_logic_vector(to_unsigned(character'pos(char_buf), 8));
+        mem(idx) <= std_logic_vector(to_unsigned(character'pos(char_buf), 8));
         idx := idx+1;
       end loop;
+      load := '0';
+      file_close(file_in);
     end if;
-    load <= '0';
-  end process;
 
+    -- if is_X(a) then
+    --   data <= (others => '-');
+    -- else
+    --   data <= mem(to_integer(unsigned(a)));
+    -- end if;
+  end process;
 end architecture;
--- src/nn02/dmem_tb.vhdl
+++ src/nn02/dmem_tb.vhdl
@@ -8,25 +8,33 @@ end entity;
 architecture testbench of dmem_tb is
   component dmem
   port (
-    a : in std_logic;
+    a : in std_logic_vector(15 downto 0);
     id : out std_logic_vector(31 downto 0);
-    cnt : out std_logic_vector(31 downto 0)
+    cnt : out std_logic_vector(31 downto 0);
+    data : out std_logic_vector(7 downto 0)
   );
   end component;
 
-  signal a : std_logic;
+  signal a : std_logic_vector(15 downto 0);
   signal id, cnt : std_logic_vector(31 downto 0);
+  signal data : std_logic_vector(7 downto 0);
 
 begin
 
   uut : dmem port map (
-    a => a,  id => id, cnt => cnt
+    a => a,  id => id, cnt => cnt, data => data
   );
 
   stim_proc : process
   begin
     wait for 20 ns;
-    a <= '0'; wait for 10 ns;
+    -- assume to be assets/train-labels-idx1-ubyte
+    a <= X"0000"; wait for 10 ns; data <= X"05";
+    a <= X"0001"; wait for 10 ns; data <= X"00";
+    a <= X"0002"; wait for 10 ns; data <= X"04";
+    a <= X"0003"; wait for 10 ns; data <= X"01";
+    -- success message
+    assert false report "end of test" severity note;
     wait;
   end process;
 end architecture;
