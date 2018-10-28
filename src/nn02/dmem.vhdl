0dfc658773c6cbe94833fcb2f5b91b1a1c720a64
knknkn <knknkn1162@gmail.com>
2018-10-28 21:27:18 +0900

    load label & image

--- src/nn02/dmem.vhdl
+++ src/nn02/dmem.vhdl
@@ -12,59 +12,84 @@ use work.dmem_const_pkg.ALL;
 use work.nn_pkg.ALL;
 
 entity dmem is
+  generic(BATCH_SIZE: natural);
   port (
     a : in std_logic_vector(15 downto 0);
-    id : out std_logic_vector(31 downto 0);
-    cnt : out std_logic_vector(31 downto 0);
+    -- [0, 60000)
+    offset : in std_logic_vector(15 downto 0);
     -- image
-    --x : out arr_type(0 to IMAGE_SIZE*IMAGE_SIZE-1);
+    x : out std_logic_vector(8*IMAGE_SIZE*IMAGE_SIZE-1 downto 0); --out arr_type(0 to IMAGE_SIZE*IMAGE_SIZE-1);
     -- [0, 9)
     t : out std_logic_vector(3 downto 0)
   );
 end entity;
 
 architecture behavior of dmem is
-  constant N : natural := 60000;
+  constant N : natural := BATCH_SIZE;
   type char_file_type is file of character;
   type labeltype is array(0 to N-1) of std_logic_vector(3 downto 0);
-  -- type imgtype is array(0 to N-1) of std_logic_vector(SIZE*IMAGE_SIZE*IMAGE_SIZE - 1 downto 0);
+  type imgtype is array(0 to N-1) of std_logic_vector(8*IMAGE_SIZE*IMAGE_SIZE-1 downto 0);
   signal label_mem : labeltype;
-  --signal data_mem : imgtype;
+  signal image_mem : imgtype;
 
 begin
-  process is
-      file label_file_in : char_file_type open read_mode is "./assets/train-labels-idx1-ubyte";
-      --file image_file_in : char_file_type open read_mode is "./assets/train-images-idx3-ubyte";
-      variable char_buf : character;
-      variable idx : natural;
+  process(offset)
+    file label_file_in : char_file_type; -- open read_mode is "./assets/train-labels-idx1-ubyte";
+    file image_file_in : char_file_type; -- open read_mode is "./assets/train-images-idx3-ubyte";
+    variable char_buf : character;
+    variable idx : natural;
   begin
-    -- initialization
-    for i in 3 downto 0 loop
-      read(label_file_in, char_buf);
-      id(i*8+7 downto i*8) <= std_logic_vector(to_unsigned(character'pos(char_buf),8));
-    end loop;
+    file_open(label_file_in, "./assets/train-labels-idx1-ubyte", READ_MODE);
+    file_open(image_file_in, "./assets/train-images-idx3-ubyte", READ_MODE);
+    if not is_X(offset) then
+      -- initialization
+      for i in 7 downto 0 loop
+        -- trash
+        read(label_file_in, char_buf);
+      end loop;
 
-    for i in 3 downto 0 loop
-      read(label_file_in, char_buf);
-      cnt(i*8+7 downto i*8) <= std_logic_vector(to_unsigned(character'pos(char_buf),8));
-    end loop;
+      for i in 15 downto 0 loop
+        -- trash
+        read(image_file_in, char_buf);
+      end loop;
 
-    idx := 0;
-    while not endfile(label_file_in) loop
-      read(label_file_in, char_buf);
-      label_mem(idx) <= std_logic_vector(to_unsigned(character'pos(char_buf), 4));
-      idx := idx + 1;
-    end loop;
+
+      idx := to_integer(unsigned(offset));
+      for i in 0 to idx-1 loop
+        read(label_file_in, char_buf);
+        for j in 0 to IMAGE_SIZE*IMAGE_SIZE-1 loop
+          read(image_file_in, char_buf);
+        end loop;
+      end loop;
+
+      for i in 0 to N-1 loop
+        read(label_file_in, char_buf);
+        label_mem(i) <= std_logic_vector(to_unsigned(character'pos(char_buf), 4));
+      end loop;
+
+      for i in 0 to N-1 loop
+        for j in IMAGE_SIZE*IMAGE_SIZE-1 downto 0 loop
+          read(image_file_in, char_buf);
+          image_mem(i)(j*8+7 downto j*8) <= std_logic_vector(to_unsigned(character'pos(char_buf), 8));
+        end loop;
+      end loop;
+    end if;
     file_close(label_file_in);
+    file_close(image_file_in);
+  end process;
 
-    loop
+  process(a)
+    variable idx : natural;
+  begin
+    if not is_X(offset) then
+      idx := to_integer(unsigned(a)-unsigned(offset));
       if is_X(a) then
-        --x <= (others => (others => '-'));
+        x <= (others => '-');
         t <= (others => '-');
       else
-        t <= label_mem(to_integer(unsigned(a)));
+        x <= image_mem(idx);
+        t <= label_mem(idx);
       end if;
-      wait on a;
-    end loop;
+    end if;
   end process;
 end architecture;
--- src/nn02/dmem_tb.vhdl
+++ src/nn02/dmem_tb.vhdl
@@ -9,43 +9,53 @@ end entity;
 
 architecture testbench of dmem_tb is
   component dmem
-  port (
-    a : in std_logic_vector(15 downto 0);
-    id : out std_logic_vector(31 downto 0);
-    cnt : out std_logic_vector(31 downto 0);
-    -- image
-    --x : out arr_type(0 to IMAGE_SIZE*IMAGE_SIZE-1);
-    -- [0, 9)
-    t : out std_logic_vector(3 downto 0)
-  );
+    generic(BATCH_SIZE: natural);
+    port (
+      a : in std_logic_vector(15 downto 0);
+      offset : in std_logic_vector(15 downto 0);
+      -- image
+      x : out std_logic_vector(8*IMAGE_SIZE*IMAGE_SIZE-1 downto 0); --out arr_type(0 to IMAGE_SIZE*IMAGE_SIZE-1);
+      -- [0, 9)
+      t : out std_logic_vector(3 downto 0)
+    );
   end component;
 
-  signal a : std_logic_vector(15 downto 0);
-  signal id, cnt : std_logic_vector(31 downto 0);
-  --signal x : arr_type(0 to IMAGE_SIZE*IMAGE_SIZE-1);
+  signal N : natural := 10;
+  signal a, offset : std_logic_vector(15 downto 0);
+  signal x : std_logic_vector(8*IMAGE_SIZE*IMAGE_SIZE-1 downto 0);
   signal t : std_logic_vector(3 downto 0);
 
 begin
-
-  uut : dmem port map (
+  uut : dmem generic map (BATCH_SIZE=>N)
+  port map (
     a => a,
-    id => id,
-    cnt => cnt,
-    --x => x,
+    offset => offset,
+    x => x,
     t => t
   );
 
   stim_proc : process
   begin
     wait for 20 ns;
-    -- id= 2049, cnt = 60000
-    assert id = X"00000801";
-    assert cnt = X"0000ea60";
+    offset <= X"0000"; wait for 10 ns;
     -- assume to be assets/train-labels-idx1-ubyte
     a <= X"0000"; wait for 10 ns; assert t = X"5";
     a <= X"0001"; wait for 10 ns; assert t = X"0";
     a <= X"0002"; wait for 10 ns; assert t = X"4";
     a <= X"0003"; wait for 10 ns; assert t = X"1";
+    a <= X"0004"; wait for 10 ns; assert t = X"9";
+    a <= X"0005"; wait for 10 ns; assert t = X"2";
+    a <= X"0006"; wait for 10 ns; assert t = X"1";
+    a <= X"0007"; wait for 10 ns; assert t = X"3";
+    offset <= X"0008"; wait for 10 ns;
+    a <= X"0008"; wait for 10 ns; assert t = X"1";
+    a <= X"0009"; wait for 10 ns; assert t = X"4";
+    a <= X"000A"; wait for 10 ns; assert t = X"3";
+    a <= X"000B"; wait for 10 ns; assert t = X"5";
+    a <= X"000C"; wait for 10 ns; assert t = X"3";
+    a <= X"000D"; wait for 10 ns; assert t = X"6";
+    a <= X"000E"; wait for 10 ns; assert t = X"1";
+    a <= X"000F"; wait for 10 ns; assert t = X"7";
     -- success message
     assert false report "end of test" severity note;
     wait;
