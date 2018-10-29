9c3388bd9828df0f757f89b97da4d1adc882579a
knknkn <knknkn1162@gmail.com>
2018-10-29 20:03:43 +0900

    impl umax testbench

--- src/nn02/forward/umax.vhdl
+++ src/nn02/forward/umax.vhdl
@@ -16,19 +16,24 @@ architecture behavior of umax is
 begin
   process(z)
     variable tmp : std_logic_vector(SIZE-1 downto 0);
-    variable res : std_logic_vector(SIZE-1 downto 0);
+    variable res : std_logic_vector(SIZE downto 0);
     variable i : natural range 0 to N-1;
   begin
-    tmp := z(0);
-    i := 0;
-    for j in 1 to N-1 loop
-      res := std_logic_vector(unsigned(tmp) - unsigned(z(j)));
-      if res(SIZE-1)='1' then
-        tmp := z(j);
-        i := j;
-      end if;
-    end loop;
-    idx <= std_logic_vector(to_unsigned(i, HSIZE));
-    zmax <= tmp;
+    if is_X(z(0)) then
+      zmax <= (others => '0');
+      idx <= (others => '0');
+    else
+      tmp := z(0);
+      i := 0;
+      for j in 1 to N-1 loop
+        res := std_logic_vector(unsigned('0' & tmp) - unsigned('0' & z(j)));
+        if res(SIZE) = '1' then
+          tmp := z(j);
+          i := j;
+        end if;
+      end loop;
+      idx <= std_logic_vector(to_unsigned(i, HSIZE));
+      zmax <= tmp;
+    end if;
   end process;
 end architecture;
