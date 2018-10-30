ab83b10f8be835aa6061d84de0c05d92c13b7b69
knknkn1162 <knknkn1162@gmail.com>
2018-10-30 16:15:23 +0900

    fix nn and nn_tb.vhdl

--- src/nn02/forward/nn.vhdl
+++ src/nn02/forward/nn.vhdl
@@ -5,7 +5,9 @@ use work.dmem_const_pkg.ALL;
 package nn_const_pkg is
   constant BATCH_SIZE : natural := 100;
   constant BSIZE : natural := 7;
-  constant INPUT_SIZE : natural := IMAGE_SIZE2-1;
+  constant INPUT_SIZE : natural := IMAGE_SIZE2;
+  constant HIDDEN_SIZE : natural := 100;
+  constant OUTPUT_SIZE : natural := 10;
 end package;
 
 library IEEE;
@@ -13,13 +15,12 @@ use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
 use work.nn_pkg.ALL;
 use work.nn_const_pkg.ALL;
-use work.two_layer_pkg.ALL;
 use work.dmem_const_pkg.ALL;
 
 entity nn is
   port (
     clk, rst : in std_logic;
-    offset : in std_logic_vector(15 downto 0);
+    offset : in std_logic_vector(TRAINING_BSIZE-1 downto 0);
     -- maybe R/W in file or memory
     w1 : in warr_type(0 to INPUT_SIZE*HIDDEN_SIZE-1);
     w2 : in warr_type(0 to HIDDEN_SIZE*OUTPUT_SIZE-1);
@@ -31,9 +32,9 @@ architecture behavior of nn is
   component dmem
     generic(BATCH_SIZE: natural);
     port (
-      a : in std_logic_vector(15 downto 0);
+      a : in std_logic_vector(TRAINING_BSIZE-1 downto 0);
       -- [0, 60000)
-      offset : in std_logic_vector(15 downto 0);
+      offset : in std_logic_vector(TRAINING_BSIZE-1 downto 0);
       -- image
       x : out arr_type(0 to IMAGE_SIZE2-1);
       -- [0, 9)
@@ -70,20 +71,15 @@ architecture behavior of nn is
     );
   end component;
 
-  signal addr : std_logic_vector(15 downto 0);
+  signal addr : std_logic_vector(TRAINING_BSIZE-1 downto 0);
   signal x : arr_type(0 to IMAGE_SIZE2-1);
   signal t : std_logic_vector(3 downto 0);
-  signal w10 : warr_type(0 to INPUT_SIZE*HIDDEN_SIZE-1);
-  signal w20 : warr_type(0 to HIDDEN_SIZE*OUTPUT_SIZE-1);
   signal z : arr_type(0 to OUTPUT_SIZE-1);
   signal idx : std_logic_vector(HSIZE-1 downto 0);
   signal zmax : std_logic_vector(SIZE-1 downto 0);
   signal ena : std_logic;
 
 begin
-  w10 <= (others => (others => '0'));
-  w20 <= (others => (others => '0'));
-
   addr_counter : counter generic map(N=>16)
   port map (
     clk => clk, rst => rst,
---
+++ src/nn02/forward/nn_tb.vhdl
@@ -0,0 +1,56 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use work.nn_const_pkg.ALL;
+use work.nn_pkg.ALL;
+use work.dmem_const_pkg.ALL;
+
+entity nn_tb is
+end entity;
+
+architecture testbench of nn_tb is
+  component nn is
+    port (
+      clk, rst : in std_logic;
+      offset : in std_logic_vector(TRAINING_BSIZE-1 downto 0);
+      -- maybe R/W in file or memory
+      w1 : in warr_type(0 to INPUT_SIZE*HIDDEN_SIZE-1);
+      w2 : in warr_type(0 to HIDDEN_SIZE*OUTPUT_SIZE-1);
+      count : out std_logic_vector(BSIZE-1 downto 0)
+    );
+  end component;
+
+  signal clk, rst : std_logic;
+  signal offset : std_logic_vector(TRAINING_BSIZE-1 downto 0);
+  signal w1 : warr_type(0 to INPUT_SIZE*HIDDEN_SIZE-1);
+  signal w2 : warr_type(0 to HIDDEN_SIZE*OUTPUT_SIZE-1);
+  signal count : std_logic_vector(BSIZE-1 downto 0);
+  signal stop : boolean;
+  constant clk_period : time := 10 ns;
+
+begin
+
+  uut : nn port map (
+    clk => clk, rst => rst,
+    offset => offset,
+    w1 => w1, w2 => w2,
+    count => count
+  );
+
+  clk_process: process
+  begin
+    while not stop loop
+      clk <= '0'; wait for clk_period/2;
+      clk <= '1'; wait for clk_period/2;
+    end loop;
+    wait;
+  end process;
+
+  stim_proc : process
+  begin
+    wait for clk_period*2;
+    -- success message
+    assert false report "end of test" severity note;
+    stop <= TRUE;
+    wait;
+  end process;
+end architecture;
