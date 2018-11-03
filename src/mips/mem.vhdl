77e9ce1bf48166f63469257e124659dea504425d
knknkn <knknkn1162@gmail.com>
2018-11-03 21:22:46 +0900

    impl ram

---
+++ src/mips/mem.vhdl
@@ -0,0 +1,55 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
+
+entity mem is
+  port (
+    clk, rst : in std_logic;
+    we : in std_logic;
+    -- program counter is 4-byte aligned
+    a : in std_logic_vector(29 downto 0);
+    wd : in std_logic_vector(31 downto 0);
+    rd : out std_logic_vector(31 downto 0)
+  );
+end entity;
+
+architecture behavior of mem is
+  constant SIZE : natural := 256; -- 0x0100
+  type ramtype is array(natural range<>) of std_logic_vector(31 downto 0);
+  signal ram : ramtype(0 to SIZE-1);
+
+begin
+  process(clk, rst, a)
+  begin
+    -- initialization
+    if rst = '1' then
+      -- text
+      -- lw $s0, 1020($0) 1000/11 00/000 1/0000 0x03FC
+      ram(0) <= X"8C1003FC";
+      -- sw $s0, 1016($0) 1010/11 00/000 1/0000 0x03F8
+      ram(1) <= X"AC1003F8";
+      -- initialize with zeros
+      ram(2 to SIZE-2) <= (others => (others => '0'));
+      -- data
+      ram(SIZE-1) <= X"00000001";
+    -- write
+    elsif rising_edge(clk) then
+      if we = '1' then
+        if not is_X(a) then
+          ram(to_integer(unsigned(a))) <= wd;
+        end if;
+      end if;
+    end if;
+  end process;
+
+  process(clk, rst, a, we)
+  begin
+    -- read
+    if is_X(a) then
+      rd <= (others => '0');
+    else
+      rd <= ram(to_integer(unsigned(a)));
+    end if;
+  end process;
+end architecture;
+
---
+++ src/mips/mem_tb.vhdl
@@ -0,0 +1,64 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
+
+entity mem_tb is
+end entity;
+
+architecture testbench of mem_tb is
+  component mem
+    port (
+      clk, rst : in std_logic;
+      we : in std_logic;
+      -- program counter is 4-byte aligned
+      a : in std_logic_vector(29 downto 0);
+      wd : in std_logic_vector(31 downto 0);
+      rd : out std_logic_vector(31 downto 0)
+    );
+  end component;
+
+  signal clk, rst, we : std_logic;
+  signal a : std_logic_vector(29 downto 0);
+  signal wd, rd : std_logic_vector(31 downto 0);
+  constant clk_period : time := 10 ns;
+  signal stop : boolean;
+
+begin
+  uut : mem port map (
+    clk => clk, rst => rst,
+    we => we,
+    a => a,
+    wd => wd,
+    rd => rd
+  );
+
+  clk_process: process
+  begin
+    while not stop loop
+      clk <= '0'; wait for clk_period/2;
+      clk <= '1'; wait for clk_period/2;
+    end loop;
+    wait;
+  end process;
+
+  stim_proc : process
+  begin
+    wait for clk_period;
+    rst <= '1'; wait for 1 ns; rst <= '0';
+    -- read test
+    a <= b"00" & X"0000000"; wait for 2 ns; assert rd /= X"00000000";
+    a <= b"00" & X"0000001"; wait for 2 ns; assert rd /= X"00000000";
+    -- clk is in rising_edge
+    a <= b"00" & X"0000002"; wait for 2 ns; assert rd = X"00000000";
+
+    -- write in ram
+    we <= '1'; a <= b"00" & X"0000002"; wd <= X"FFFFFFFF"; wait for clk_period;
+    we <= '0'; wait for 1 ns; assert rd = X"FFFFFFFF";
+    -- skip
+    stop <= TRUE;
+    -- success message
+    assert false report "end of test" severity note;
+    wait;
+  end process;
+
+end architecture;
