0ebafd1aaaf3a4e85717ecc3b454b62dae77050e
knknkn <knknkn1162@gmail.com>
2018-11-04 23:36:01 +0900

    organize mips testbench

--- src/mips/mips.vhdl
+++ src/mips/mips.vhdl
@@ -5,16 +5,6 @@ use IEEE.NUMERIC_STD.ALL;
 entity mips is
   port (
     clk, rst : in std_logic;
-    -- controller
-    -- for memadr
-    pc_aluout_s, pc_en : in std_logic;
-    -- for memwrite
-    mem_we: in std_logic;
-    -- for writeback
-    instr_en, reg_we : in std_logic;
-    -- for memadr
-    alucont : in std_logic_vector(2 downto 0);
-    rt_imm_s : in std_logic;
     -- scan for testbench
     pc : out std_logic_vector(31 downto 0);
     pcnext : out std_logic_vector(31 downto 0);
@@ -55,9 +45,9 @@ architecture behavior of mips is
     port (
       clk, rst : in std_logic;
       mem_rd : in std_logic_vector(31 downto 0);
-      rs, rt : out std_logic_vector(31 downto 0);
-      imm : out std_logic_vector(31 downto 0);
+      rs, rt, imm : out std_logic_vector(31 downto 0);
       -- controller
+      opcode, funct : out std_logic_vector(5 downto 0);
       instr_en, reg_we : in std_logic;
       -- scan
       reg_wa : out std_logic_vector(4 downto 0);
@@ -76,13 +66,55 @@ architecture behavior of mips is
       rt_imm_s : in std_logic
     );
   end component;
+  component controller
+    port (
+      clk, rst : in std_logic;
+      opcode, funct : in std_logic_vector(5 downto 0);
+      -- for memadr
+      pc_aluout_s, pc_en : out std_logic;
+      -- for memwrite
+      mem_we: out std_logic;
+      -- for writeback
+      instr_en, reg_we : out std_logic;
+      -- for memadr
+      alucont : out std_logic_vector(2 downto 0);
+      rt_imm_s : out std_logic
+    );
+  end component;
 
   signal alures0 : std_logic_vector(31 downto 0);
   signal mem_rd0, mem_wd0, mem_addr0 : std_logic_vector(31 downto 0);
   signal rs0, rt0, imm0 : std_logic_vector(31 downto 0);
   signal zero0 : std_logic;
 
+  -- controller
+  signal opcode, funct : std_logic_vector(5 downto 0);
+  -- for memadr
+  signal pc_aluout_s, pc_en : std_logic;
+  -- for memwrite
+  signal mem_we: std_logic;
+  -- for writeback
+  signal instr_en, reg_we : std_logic;
+  -- for memadr
+  signal alucont : std_logic_vector(2 downto 0);
+  signal rt_imm_s : std_logic;
+
 begin
+  controller0 : controller port map (
+    clk => clk, rst => rst,
+    opcode => opcode, funct => funct,
+    -- out
+    -- for memadr
+    pc_aluout_s => pc_aluout_s, pc_en => pc_en,
+    -- for memwrite
+    mem_we => mem_we,
+    -- for writeback
+    instr_en => instr_en, reg_we => reg_we,
+    -- for memadr
+    alucont => alucont,
+    rt_imm_s => rt_imm_s
+  );
+
   memadr0 : memadr port map (
     clk => clk, rst => rst,
     alures => alures0,
@@ -95,7 +127,7 @@ begin
     clk => clk, rst => rst,
     addr => mem_addr0,
     wd => mem_wd0,
-    rd => mem_rd0,
+    rd => mem_rd0, -- instr0
     we => mem_we
   );
   addr <= mem_addr0;
@@ -105,6 +137,7 @@ begin
     clk => clk, rst => rst,
     mem_rd => mem_rd0,
     rs => rs0, rt => rt0, imm => imm0,
+    opcode => opcode, funct => funct,
     instr_en => instr_en, reg_we => reg_we,
     reg_wa => reg_wa, reg_wd => reg_wd
   );
