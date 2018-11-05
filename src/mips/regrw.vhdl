1894d333305862293ddd0be31c79c95420980eb8
knknkn1162 <knknkn1162@gmail.com>
2018-11-05 16:35:59 +0900

    impl regrw

---
+++ src/mips/regrw.vhdl
@@ -0,0 +1,51 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity regrw is
+  port (
+    clk, rst : in std_logic;
+    rs, rt : in std_logic_vector(4 downto 0);
+    wd : in std_logic_vector(31 downto 0);
+    imm : in std_logic_vector(15 downto 0);
+
+    rds, rdt, immext : out std_logic_vector(31 downto 0);
+    -- controller
+    we : in std_logic
+  );
+end entity;
+
+architecture behavior of regrw is
+  component regfile
+    port (
+      clk, rst : in std_logic;
+      -- 25:21(read)
+      a1 : in std_logic_vector(4 downto 0);
+      rd1 : out std_logic_vector(31 downto 0);
+      -- 20:16(read)
+      a2 : in std_logic_vector(4 downto 0);
+      rd2 : out std_logic_vector(31 downto 0);
+
+      a3 : in std_logic_vector(4 downto 0);
+      wd3 : in std_logic_vector(31 downto 0);
+      we3 : in std_logic
+    );
+  end component;
+
+  component sgnext
+    port (
+      a : in std_logic_vector(15 downto 0);
+      y : out std_logic_vector(31 downto 0)
+        );
+  end component;
+begin
+  regfile0 : regfile port map (
+    clk => clk, rst => rst,
+    a1 => rs, rd1 => rds,
+    a2 => rt, rd2 => rdt,
+    a3 => rt, wd3 => wd, we3 => we
+  );
+  sgnext0 : sgnext port map (
+    a => imm,
+    y => immext
+  );
+end architecture;
---
+++ src/mips/regrw_tb.vhdl
@@ -0,0 +1,70 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity regrw_tb is
+end entity;
+
+architecture testbench of regrw_tb is
+  component regrw
+    port (
+      clk, rst : in std_logic;
+      rs, rt : in std_logic_vector(4 downto 0);
+      wd : in std_logic_vector(31 downto 0);
+      imm : in std_logic_vector(15 downto 0);
+
+      rds, rdt, immext : out std_logic_vector(31 downto 0);
+      -- controller
+      we : in std_logic
+    );
+  end component;
+
+  signal clk, rst : std_logic;
+  signal rs, rt : std_logic_vector(4 downto 0);
+  signal wd : std_logic_vector(31 downto 0);
+  signal imm : std_logic_vector(15 downto 0);
+  signal rds, rdt, immext : std_logic_vector(31 downto 0);
+  -- controller
+  signal we : std_logic;
+
+  constant clk_period : time := 10 ns;
+  signal stop : boolean;
+
+begin
+  uut : regrw port map (
+    clk => clk, rst => rst,
+    rs => rs, rt => rt,
+    wd => wd,
+    imm => imm,
+    rds => rds, rdt => rdt, immext => immext,
+    we => we
+  );
+
+  clk_process: process
+  begin
+    while not stop loop
+      clk <= '0'; wait for clk_period/2;
+      clk <= '1'; wait for clk_period/2;
+    end loop;
+    wait;
+  end process;
+
+  stim_proc : process
+  begin
+    wait for clk_period;
+    rst <= '1'; we <= '0'; wait for 1 ns; rst <= '0';
+
+    -- mem writeback
+    rt <= "00001"; we <= '1'; wd <= X"0000000A"; wait for clk_period/2;
+    -- check whether the data is written
+    rt <= "00001"; we <= '0'; wait for clk_period;
+    assert rdt = X"0000000A";
+
+    imm <= X"0020"; wait for 1 ns; assert immext = X"00000020";
+
+    -- skip
+    stop <= TRUE;
+    -- success message
+    assert false report "end of test" severity note;
+    wait;
+  end process;
+end architecture;
