98cc2aee2925c49560dc1c8d97fed0107822d7cb
knknkn <knknkn1162@gmail.com>
2018-11-17 13:04:10 +0900

    improve instr_shift_register

--- src/mips/instr_shift_register.vhdl
+++ src/mips/instr_shift_register.vhdl
@@ -15,13 +15,15 @@ entity instr_shift_register is
 end entity;
 
 architecture behavior of instr_shift_register is
-  component flopr_en
-    generic(N : natural := 32);
+  component shift_register2
+    generic(N: natural);
     port (
-      clk, rst, en: in std_logic;
-      a : in std_logic_vector(N-1 downto 0);
-      y : out std_logic_vector(N-1 downto 0)
-        );
+      clk, rst : in std_logic;
+      en : in std_logic_vector(1 downto 0);
+      a0 : in std_logic_vector(N-1 downto 0);
+      a1 : out std_logic_vector(N-1 downto 0);
+      a2 : out std_logic_vector(N-1 downto 0)
+    );
   end component;
 
   -- opcode, rs, rt, rd, funct
@@ -34,16 +36,11 @@ architecture behavior of instr_shift_register is
 begin
   x0 <= opcode0 & rs0 & rt0 & rd0 & funct0;
 
-  flopr_en0 : flopr_en generic map(N=>N)
+  shift_register0 : shift_register2 generic map (N=>N)
   port map (
-    clk => clk, rst => rst, en => en(0),
-    a => x0, y => x1
-  );
-
-  flopr_en1 : flopr_en generic map(N=>N)
-  port map (
-    clk => clk, rst => rst, en => en(1),
-    a => x1, y => x2
+    clk => clk, rst => rst,
+    en => en,
+    a0 => x0, a1 => x1, a2 => x2
   );
 
   opcode1 <= x1(26 downto 21); rs1 <= x1(20 downto 16); rt1 <= x1(15 downto 11); rd1 <= x1(10 downto 6); funct1 <= x1(5 downto 0);
