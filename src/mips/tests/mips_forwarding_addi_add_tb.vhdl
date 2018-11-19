998a0a9630810ff5c0f58edeece310fab21182da
knknkn <knknkn1162@gmail.com>
2018-11-19 20:39:05 +0900

    fix forwarding_addi_add testbench

--- src/mips/tests/mips_forwarding_addi_add_tb.vhdl
+++ src/mips/tests/mips_forwarding_addi_add_tb.vhdl
@@ -15,13 +15,14 @@ architecture testbench of mips_forwarding_addi_add_tb is
       pc : out std_logic_vector(31 downto 0);
       pcnext : out std_logic_vector(31 downto 0);
       addr, mem_rd, mem_wd : out std_logic_vector(31 downto 0);
-      reg_wa : out std_logic_vector(4 downto 0);
-      reg_wd : out std_logic_vector(31 downto 0);
       rds, rdt, immext : out std_logic_vector(31 downto 0);
       ja : out std_logic_vector(27 downto 0);
       alures : out std_logic_vector(31 downto 0);
       -- for scan
       dec_sa, dec_sb : out state_vector_type;
+      reg_wa : out std_logic_vector(4 downto 0);
+      reg_wd : out std_logic_vector(31 downto 0);
+      reg_we : out std_logic;
       -- -- check stall or not
       stall_en : out std_logic
     );
@@ -32,6 +33,7 @@ architecture testbench of mips_forwarding_addi_add_tb is
   signal addr, mem_rd, mem_wd : std_logic_vector(31 downto 0);
   signal reg_wa : std_logic_vector(4 downto 0);
   signal reg_wd : std_logic_vector(31 downto 0);
+  signal reg_we : std_logic;
   signal rds, rdt, immext : std_logic_vector(31 downto 0);
   signal ja : std_logic_vector(27 downto 0);
   signal alures : std_logic_vector(31 downto 0);
@@ -48,12 +50,12 @@ begin
     clk => clk, rst => rst, load => load,
     pc => pc, pcnext => pcnext,
     addr => addr, mem_rd => mem_rd, mem_wd => mem_wd,
-    reg_wa => reg_wa,
-    reg_wd => reg_wd,
     rds => rds, rdt => rdt, immext => immext,
     ja => ja,
     alures => alures,
+    -- for scan
     dec_sa => dec_sa, dec_sb => dec_sb,
+    reg_wa => reg_wa, reg_wd => reg_wd, reg_we => reg_we,
     stall_en => stall_en
   );
 
@@ -107,16 +109,14 @@ begin
     wait for clk_period;
 
     -- (AddiWriteBackS, CalcS(RtypeCalcS))
-    assert dec_sa = CONST_REGWBS; assert dec_sb = CONST_CALCS;
-    -- AddiWriteBackS : addi $s0, $0, 5
-    assert reg_wa = "10000"; assert reg_wd = X"00000005";
+     assert dec_sb = CONST_CALCS;
     -- CalcS : add $s1, $s0, $s0
     assert alures = X"0000000A";
     wait for clk_period;
 
-    -- (FetchS, ALUWriteBackS)
-    -- ALUWriteBackS : add $s1, $s0, $s0
-    assert dec_sa = CONST_FETCHS; assert dec_sb = CONST_REGWBS;
+    assert reg_wa = "10000"; assert reg_wd = X"00000005";
+    wait for clk_period;
+
     assert reg_wa = "10001"; assert reg_wd = X"0000000A";
 
     assert false report "end of test" severity note;
