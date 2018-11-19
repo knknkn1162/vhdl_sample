bccb51f147ba14a2d51e0006b25bfe0bf3f1907c
knknkn <knknkn1162@gmail.com>
2018-11-19 09:59:33 +0900

    regw_cache with shift_register2_load

--- src/mips/regw_cache.vhdl
+++ src/mips/regw_cache.vhdl
@@ -25,61 +25,40 @@ entity regw_cache is
 end entity;
 
 architecture behavior of regw_cache is
-  component flopr_en
-    generic(N : natural);
+  component shift_register2_load is
+    generic(N: natural);
     port (
-      clk, rst, en: in std_logic;
-      a : in std_logic_vector(N-1 downto 0);
-      y : out std_logic_vector(N-1 downto 0)
-        );
-  end component;
-
-  component mux2
-    generic(N : integer);
-    port (
-      d0 : in std_logic_vector(N-1 downto 0);
-      d1 : in std_logic_vector(N-1 downto 0);
+      clk, rst : in std_logic;
+      en : in std_logic_vector(1 downto 0);
+      load0, load1 : in std_logic_vector(N-1 downto 0);
       s : in std_logic;
-      y : out std_logic_vector(N-1 downto 0)
-        );
+      b1, b2 : out std_logic_vector(N-1 downto 0)
+    );
   end component;
 
   constant WD_DIM : natural := 38;
   signal calcd, calcd0 : std_logic_vector(WD_DIM-1 downto 0);
   signal memrd : std_logic_vector(WD_DIM-1 downto 0);
-  signal data1, data2 : std_logic_vector(WD_DIM-1 downto 0);
+  signal data1 : std_logic_vector(WD_DIM-1 downto 0);
   signal regd0 : std_logic_vector(WD_DIM-1 downto 0);
   signal reg_wa0, memrds_wa0 : std_logic_vector(4 downto 0);
   signal reg_wd0, memrds_wd0 : std_logic_vector(31 downto 0);
   signal reg_we0, memrds_we0 : std_logic;
 begin
+
   calcd <= calcs_we & calcs_wd & calcs_wa;
   memrd <= memrds_we & memrds_wd & memrds_wa;
 
-  flopr_en0 : flopr_en generic map(N=>WD_DIM)
-  port map (
-    clk => clk, rst => rst, en => '1',
-    a => calcd,
-    y => calcd0
-  );
-
-  calcs_memrds_mux : mux2 generic map(N=>WD_DIM)
-  port map (
-    d0 => calcd0,
-    d1 => memrd,
+  shift_register2_load0 : shift_register2_load generic map(N=>WD_DIM)
+  port map(
+    clk => clk, rst => rst, en => "11",
+    load0 => calcd, load1 => memrd,
     s => memrds_load_s,
-    y => data1
+    b1 => data1, b2 => regd0
   );
   memrds_wa0 <= data1(4 downto 0);
   memrds_wd0 <= data1(WD_DIM-2 downto 5);
   memrds_we0 <= data1(WD_DIM-1);
-
-  flopr_en1 : flopr_en generic map(N=>WD_DIM)
-  port map (
-    clk => clk, rst => rst, en => '1',
-    a => data1,
-    y => regd0
-  );
   reg_wa0 <= regd0(4 downto 0); reg_wa <= reg_wa0;
   reg_wd0 <= regd0(WD_DIM-2 downto 5); reg_wd <= reg_wd0;
   reg_we0 <= regd0(WD_DIM-1); reg_we <= reg_we0;
