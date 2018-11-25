6e4aadda099a1c99d9823850f6a818f7d8b2a77e
knknkn <knknkn1162@gmail.com>
2018-11-25 14:47:01 +0900

    delete is_init

--- src/mips/state_pkg.vhdl
+++ src/mips/state_pkg.vhdl
@@ -12,19 +12,4 @@ package state_pkg is
     AddiCalcS,
     UnknownS
   );
-
-  function is_init(state: statetype) return std_logic;
 end package;
-
-package body state_pkg is
-  function is_init(state: statetype) return std_logic is
-    variable res : std_logic;
-  begin
-    if res = Wait4S or res = Wait3S or res = Wait2S or res = WaitS or res = InitS or res = LoadS then
-      res = '1';
-    else
-      res = '0';
-    end if;
-    return res;
-  end function;
-end package body;
